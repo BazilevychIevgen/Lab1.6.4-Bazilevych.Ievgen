b50fce5ce73a5d42119086e3b329a801bf54f26b
Admin <baro@ikrt>
2016-02-22 15:31:44 +0200

    Lab1.6.4

---
+++ src/edu/brainacad/Main.java
@@ -0,0 +1,24 @@
+package edu.brainacad;
+
+import java.util.Arrays;
+
+/**
+ * Created by Admin on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {1,2,4,5,6,7,8,55,33,44,55,44,33};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+        int searchVal = 33;
+        int retVal = Arrays.binarySearch(arr,searchVal);
+        System.out.println(index 33 is  + retVal);
+    }
+
+}
